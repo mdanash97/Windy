765dd65429f838cd79f165eaa261525e536d97e9
mdanash97 <m.elamirdanash@gmail.com>
2023-06-25 13:14:18 +0300

    first steps

---
+++ app/src/main/java/com/example/windy/WeatherDTO.kt
@@ -0,0 +1,108 @@
+package com.example.windy
+
+data class WeatherData(
+    val lat: Double,
+    val lon: Double,
+    val timezone: String,
+    val timezone_offset: Int,
+    val current: CurrentWeatherData,
+    val minutely: List<MinutelyWeatherData>,
+    val hourly: List<HourlyWeatherData>,
+    val daily: List<DailyWeatherData>,
+    val alerts: List<Alert>
+)
+
+data class CurrentWeatherData(
+    val dt: Long,
+    val sunrise: Long,
+    val sunset: Long,
+    val temp: Double,
+    val feels_like: Double,
+    val pressure: Int,
+    val humidity: Int,
+    val dew_point: Double,
+    val uvi: Double,
+    val clouds: Int,
+    val visibility: Int,
+    val wind_speed: Double,
+    val wind_deg: Int,
+    val wind_gust: Double,
+    val weather: List<Weather>
+)
+
+data class MinutelyWeatherData(
+    val dt: Long,
+    val precipitation: Int
+)
+
+data class HourlyWeatherData(
+    val dt: Long,
+    val temp: Double,
+    val feels_like: Double,
+    val pressure: Int,
+    val humidity: Int,
+    val dew_point: Double,
+    val uvi: Double,
+    val clouds: Int,
+    val visibility: Int,
+    val wind_speed: Double,
+    val wind_deg: Int,
+    val wind_gust: Double,
+    val weather: List<Weather>,
+    val pop: Double
+)
+
+data class DailyWeatherData(
+    val dt: Long,
+    val sunrise: Long,
+    val sunset: Long,
+    val moonrise: Long,
+    val moonset: Long,
+    val moon_phase: Double,
+    val summary: String,
+    val temp: TempData,
+    val feels_like: FeelsLikeData,
+    val pressure: Int,
+    val humidity: Int,
+    val dew_point: Double,
+    val wind_speed: Double,
+    val wind_deg: Int,
+    val wind_gust: Double,
+    val weather: List<Weather>,
+    val clouds: Int,
+    val pop: Double,
+    val rain: Double?,
+    val uvi: Double
+)
+
+data class TempData(
+    val day: Double,
+    val min: Double,
+    val max: Double,
+    val night: Double,
+    val eve: Double,
+    val morn: Double
+)
+
+data class FeelsLikeData(
+    val day: Double,
+    val night: Double,
+    val eve: Double,
+    val morn: Double
+)
+
+data class Weather(
+    val id: Int,
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class Alert(
+    val sender_name: String,
+    val event: String,
+    val start: Long,
+    val end: Long,
+    val description: String,
+    val tags: List<String>
+)
